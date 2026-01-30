3f5f70780ae7e3b959014607e41de37e7038e2ea
muirfer <muirfer@gmail.com>
2026-01-30 09:41:35 -0300

    Article task list and initial version of mass creation

---
+++ js/views/creation_articlelist.js
@@ -0,0 +1,216 @@
+(function ($) {
+    'use strict';
+
+    // Ensure global App namespace exists
+    window.App = window.App || {};
+
+    // Module for handling Reporting Leadtime logic
+    var CreationArticleList = {
+        /**
+         * Initializes the DataTable for the Reporting Leadtime main table.
+         * Handles table initialization, DOM manipulation, and DataTable configuration.
+         */
+        datatableCreationInit: function () {
+            var $table = $('#datatable-cr');
+
+            if ($.fn.DataTable.isDataTable($table)) {
+                $table.DataTable().destroy();
+            }
+
+            $table.on('init.dt', function () {
+                var $wrapper = $table.closest('.dataTables_wrapper');
+
+                // Filter
+                var $filter = $wrapper.find('.dataTables_filter');
+                $filter.parent().removeClass("col-lg-6").addClass("col-lg-12");
+                $filter.css("text-align", "right");
+                var $searchWrapper = $('<div id="search-cr"></div>').append($filter.detach());
+                $('#searchDiv').append($searchWrapper);
+
+                // Length
+                var $len = $wrapper.find('.dataTables_length');
+                var $select = $len.find('select');
+
+                $select.detach().show().addClass("custom-select form-control");
+                var $pagWrapper = $('<div id="pag-cr"></div>').append($select);
+                $('#divPag').append($pagWrapper);
+
+                if ($.isFunction($.fn['select2'])) {
+                    $select.select2({
+                        theme: 'bootstrap',
+                        minimumResultsForSearch: -1
+                    });
+                }
+
+                // Remove original container
+                $len.parent().parent().remove();
+
+                // Pagination
+                var $pag = $wrapper.find('.paging_simple_numbers');
+                var $pagWrapper = $('<div id="pagination-cr"></div>').append($pag.detach());
+                $('#fPag').append($pagWrapper);
+            });
+
+            $table.dataTable({
+                dom: '<"row"<"col-lg-6"l><"col-lg-6"f>><"table-responsive"t>p'
+            });
+
+            $("#cr1").css("width", "5%")
+            $("#cr2").css("width", "5%")
+            $("#cr3").css("width", "20%")
+            $("#cr4").css("width", "15%")
+            $("#cr5").css("width", "15%")
+            $("#cr6").css("width", "9%")
+            $("#cr7").css("width", "7%")
+            $("#cr8").css("width", "10%")
+            $("#cr9").css("width", "5%")
+            $("#cr10").css("width", "5%")
+            $("#cr11").css("width", "4%")
+        },
+        /**
+         * Initializes the DataTable for the Reporting Leadtime details table.
+         * Handles table initialization, DOM manipulation, and DataTable configuration.
+         */
+        datatableUpdateInit: function () {
+            var $table = $('#datatable-up');
+
+            if ($.fn.DataTable.isDataTable($table)) {
+                $table.DataTable().destroy();
+            }
+
+            $table.on('init.dt', function () {
+                var $wrapper = $table.closest('.dataTables_wrapper');
+
+                // Filter
+                var $filter = $wrapper.find('.dataTables_filter');
+                $filter.parent().removeClass("col-lg-6").addClass("col-lg-12");
+                $filter.css("text-align", "right");
+                var $searchWrapper = $('<div id="search-up" style="display:none"></div>').append($filter.detach());
+                $('#searchDiv').append($searchWrapper);
+
+                // Length
+                var $len = $wrapper.find('.dataTables_length');
+                var $select = $len.find('select');
+
+                $select.detach().show().addClass("custom-select form-control");
+                var $pagWrapper = $('<div id="pag-up" style="display:none"></div>').append($select);
+                $('#divPag').append($pagWrapper);
+
+                if ($.isFunction($.fn['select2'])) {
+                    $select.select2({
+                        theme: 'bootstrap',
+                        minimumResultsForSearch: -1
+                    });
+                }
+
+                // Remove original container
+                $len.parent().parent().remove();
+
+                // Pagination
+                var $pag = $wrapper.find('.paging_simple_numbers');
+                var $pagWrapper = $('<div id="pagination-up" style="display:none"></div>').append($pag.detach());
+                $('#fPag').append($pagWrapper);
+            });
+
+            $table.dataTable({
+                dom: '<"row"<"col-lg-6"l><"col-lg-6"f>><"table-responsive"t>p'
+            });
+
+            $("#c1").css("width", "5%")
+            $("#c1a").css("width", "5%")
+            $("#c2").css("width", "19%")
+            $("#c3").css("width", "16%")
+            $("#c4").css("width", "12%")
+            $("#c5").css("width", "13%")
+            $("#c6").css("width", "15%")
+            $("#c7").css("width", "8%")
+            $("#c8").css("width", "3%")
+            $("#c9").css("width", "2%")
+            $("#c10").css("width", "2%")
+        },
+        /**
+         * Filters by status of the product.
+         * @param {string} sts - The status to select.
+         */
+        selectStatus: function (sts) {
+            if (sts === "all") {
+                $(".sts1").show();
+                $(".sts2").show();
+            } else if (sts === "av") {
+                $(".sts1").show();
+                $(".sts2").hide();
+            } else if (sts === "us") {
+                $(".sts1").hide();
+                $(".sts2").show();
+            }
+        },
+        /**
+         * Switches between my tasks and all tasks.
+         * @param {boolean} myTasks - true for my tasks, false for all tasks.
+         */
+        openMyTasks: function (myTasks) {
+            if (myTasks) {
+                $("#btnMyTasks").addClass("btn-success")
+                $("#btnTasks").removeClass("btn-success")
+                $("#countC").text("3")
+                $("#countU").text("2")
+                $(".teamT").hide()
+            } else {
+                $("#btnMyTasks").removeClass("btn-success")
+                $("#btnTasks").addClass("btn-success")
+                $("#countC").text("5")
+                $("#countU").text("6")
+                $(".teamT").show()
+            }
+        },
+        /**
+         * Switches between create and update tasks.
+         * @param {boolean} cTasks - true for create tasks, false for update tasks.
+         */
+        openCreationTasks: function (cTasks) {
+            if (cTasks) {
+                $("#btnCTasks").addClass("btn-info")
+                $("#btnUTasks").removeClass("btn-info")
+                $(".oCreate").show()
+                $(".oUpdate").hide()
+                $("#datatable-cr").show()
+                $("#datatable-up").hide()
+                $("#pag-cr").show()
+                $("#pag-up").hide()
+                $("#search-cr").show()
+                $("#search-up").hide()
+                $("#pagination-cr").show()
+                $("#pagination-up").hide()
+
+            } else {
+                $("#btnCTasks").removeClass("btn-info")
+                $("#btnUTasks").addClass("btn-info")
+                $(".oCreate").hide()
+                $(".oUpdate").show()
+                $("#datatable-cr").hide()
+                $("#datatable-up").show()
+                $("#pag-cr").hide()
+                $("#pag-up").show()
+                $("#search-cr").hide()
+                $("#search-up").show()
+                $("#pagination-cr").hide()
+                $("#pagination-up").show()
+            }
+        },
+        // Helper methods for data operations (Placeholders)
+        getItems: function () {
+            console.log("getItems called - Refreshing data...");
+        },
+
+        clearFilters: function () {
+            console.log("clearFilters called - Resetting filters...");
+        },
+
+        exportExcel: function () {
+            console.log("exportExcel called - Downloading report...");
+        }
+    };
+
+    App.CreationArticleList = CreationArticleList;
+
+})(jQuery);
---
+++ js/views/creation_create_tasks.js
@@ -0,0 +1,599 @@
+(function ($) {
+
+	'use strict';
+
+	// Ensure global App namespace exists
+	window.App = window.App || {};
+
+	var CreationCreateTasks = {
+
+		// State variables
+		isSingleSelection: true,
+		tableCount: 0,
+		invalidGtinForSingleTest: "12345678901234",
+		modeGtinInput: true,
+		reserveGtinsFor: [],
+
+		initialize: function () {
+			this.initDatatable();
+			this.initWizard();
+			this.initInlineEvents();
+		},
+
+		initDatatable: function () {
+			var $table = $('#datatable-gi');
+
+			if ($.fn.DataTable.isDataTable($table)) {
+				$table.DataTable().destroy();
+			}
+
+			$table.on('init.dt', function () {
+				var $wrapper = $table.closest('.dataTables_wrapper');
+
+				// Filter
+				var $filter = $wrapper.find('.dataTables_filter');
+				$filter.parent().removeClass("col-lg-6").addClass("col-lg-12");
+				$filter.css("text-align", "right");
+				$('#searchDiv').append($filter.detach());
+
+				// Length
+				var $len = $wrapper.find('.dataTables_length');
+				var $select = $len.find('select');
+
+				$select.detach().show().addClass("custom-select form-control");
+				$('#divPag').append($select);
+
+				if ($.isFunction($.fn['select2'])) {
+					$select.select2({
+						theme: 'bootstrap',
+						minimumResultsForSearch: -1
+					});
+				}
+
+				// Remove original container
+				$len.parent().parent().remove();
+
+				// Pagination
+				var $pag = $wrapper.find('.paging_simple_numbers');
+				$('#fPag').append($pag.detach());
+			});
+
+			$table.dataTable({
+				dom: '<"row"<"col-lg-6"l><"col-lg-6"f>><"table-responsive"t>p'
+			});
+
+			$("#thStatus").css("width", "5%");
+			$("#thGtin").css("width", "25%");
+			$("#thProcess").css("width", "18%");
+			$("#thMes").css("width", "37%");
+			$("#thActions").css("width", "15%");
+
+			$('#datatable-gi').DataTable().page.len(6).draw();
+		},
+
+		initWizard: function () {
+			var self = this;
+			/*
+			Wizard #4
+			*/
+			$('#w4').bootstrapWizard({
+				tabClass: 'wizard-steps',
+				nextSelector: 'ul.pager li.next',
+				previousSelector: 'ul.pager li.previous',
+				firstSelector: null,
+				lastSelector: null,
+				onNext: function (tab, navigation, index, newindex) {
+					// Validation logic commented out in original file
+				},
+				onTabClick: function (tab, navigation, index, newindex) {
+					// Logic commented out in original file
+				},
+				onTabChange: function (tab, navigation, index, newindex) {
+					// Logic commented out in original file
+				},
+				onTabShow: function (tab, navigation, index) {
+					var $current = index;
+
+					navigation.find('li').removeClass('active');
+					navigation.find('li').eq($current).addClass('active');
+
+					if (index === 0) {
+						$("#btnRegNext").hide();
+						$("#btnNext_input").show();
+						if ($("#lblBrandType").text() === "D1 - Delhaize") {
+							$("#btnNext_res").show();
+						}
+						$(".previous").hide();
+						$(".gtinInputPanel").hide();
+						$(".gtinInputPanel2").hide();
+						$(".supRegInfoPanel").hide();
+						$(".supNoRegInfoPanel").hide();
+					} else {
+						$("#btnRegNext").show();
+						$("#btnNext_input").hide();
+						$("#btnNext_res").hide();
+						$(".previous").show();
+						$(".gtinInputPanel").show();
+
+						if ($("#lblGtinInput").is(":visible") && $("#lblGtinInput").text() === "-") {
+							$("#pDetails").hide();
+							$("#pNoDetailsMsge").show();
+						} else {
+							$("#pDetails").show();
+							$("#pNoDetailsMsge").hide();
+						}
+
+						if (index === 3) {
+							$("#btnRegNext").hide();
+							$(".btnAction").show();
+						} else {
+							$("#btnRegNext").show();
+							$(".btnAction").hide();
+						}
+					}
+				}
+			});
+		},
+
+		initInlineEvents: function () {
+			var self = this;
+			$("#mainSection").css("background", "white");
+
+			$("#gtinInput").on('keyup', function (e) {
+				e.stopPropagation();
+				e.preventDefault();
+
+				if (e.key === 'Enter' || e.keyCode === 13) {
+					self.pasteSingleGtin();
+				}
+
+				return false;
+			});
+
+			$("#supInput").on('keyup', function (e) {
+				e.stopPropagation();
+				e.preventDefault();
+
+				if (e.key === 'Enter' || e.keyCode === 13) {
+					$('.supRow:hidden').first().show();
+					$("#panelSupplierDetails").show();
+
+					if ($(".supRegInfoPanel").is(":visible")) {
+						//means that a non-regular supplier was entered
+						$(".supNoRegInfoPanel").show();
+						$('.lblOtherSup:hidden').first().show();
+					} else {
+						$(".supRegInfoPanel").show();
+					}
+
+					$("#supInput").val("");
+					$("#supInput").focus();
+				}
+
+				return false;
+			});
+
+
+			var gtinInput = document.getElementById('gtinInput');
+			if (gtinInput) {
+				gtinInput.addEventListener('paste', function (e) {
+					self.handlePaste(e);
+				});
+			}
+			$("#gtinInput").removeAttr("maxLength");
+
+			document.addEventListener('keydown', function (event) {
+				if (event.keyCode === 13) {
+					event.preventDefault();
+					event.stopPropagation();
+				}
+			});
+
+			$(".main-panel").css("height", "calc(90vh - " + $(".main-panel").offset().top + "px)");
+		},
+
+		expandAll: function () {
+			$(".card-body").css("display", "block");
+		},
+
+		collapseAll: function () {
+			//$(".collapse").collapse();                                
+			$(".card-body").css("display", "none");
+			$("html, body").animate({ scrollTop: 0 }, "slow");
+		},
+
+		tsvStringToArray: function (data) {
+			const re = /(\t|\r?\n|\r|^)(?:"([^"]*(?:""[^"]*)*)"|([^\t\r\n]*))/gi;
+			const result = [[]];
+			let matches;
+			while ((matches = re.exec(data))) {
+				if (matches[1].length && matches[1] !== "\t") result.push([]);
+				result[result.length - 1].push(
+					matches[2] !== undefined ? matches[2].replace(/""/g, '"') : matches[3]
+				);
+			}
+			//console.log(result);
+			return result;
+		},
+
+		handlePaste: function (e) {
+			var clipboardData, pastedData;
+
+			// Stop data actually being pasted into div
+			e.stopPropagation();
+			e.preventDefault();
+
+			// Get pasted data via clipboard API
+			clipboardData = e.clipboardData || window.clipboardData;
+			pastedData = clipboardData.getData('Text');
+
+			// Do whatever with pasteddata                
+			var gtinArray = this.tsvStringToArray(pastedData);
+			if (gtinArray.length > 1) {
+				//paste all
+				this.pasteGtins();
+			} else {
+				this.pasteSingleGtin();
+			}
+
+			this.doOnGtinListUpdate();
+		},
+
+		handlePasteFromButton: function () {
+			//paste all
+			// $('.gtinRow').show();
+			// $("#panelMassValidation").show();   
+			// $("#gtinInput").focus();
+
+			this.pasteGtins();
+		},
+
+		doOnGtinListUpdate: function () {
+			//$("#fPag").css("display",tableCount > 5 ? "block" : "none");              
+		},
+
+		toogleBrandType: function (btn, otherBtnId) {
+
+			if ($(btn).hasClass("btn-default")) {
+				//switch back to default
+				$(btn).removeClass("btn-default");
+				$(btn).addClass("btn-primary");
+				$("#" + otherBtnId).removeClass("btn-primary");
+				$("#" + otherBtnId).addClass("btn-default");
+			}
+
+			//update UoM                
+			if (otherBtnId === "btnN1") {
+				//Active one is D1
+				$("#lblBrandType").text("D1 - Delhaize");
+				$("#btnNext_res").show();
+			} else {
+				//Active one is N1
+				$("#lblBrandType").text("N1 - National");
+				$("#btnNext_res").hide();
+			}
+		},
+
+		toogleGDSNType: function (btn, otherBtnId) {
+
+			if ($(btn).hasClass("btn-default")) {
+				//switch back to default
+				$(btn).removeClass("btn-default");
+				$(btn).addClass("btn-primary");
+				$("#" + otherBtnId).removeClass("btn-primary");
+				$("#" + otherBtnId).addClass("btn-default");
+			}
+
+			//update UoM                
+			if (otherBtnId === "btnGS1") {
+				//Active one is No-GDSN
+				$("#lblType").text("Manual");
+				$(".nonGdsn").show();
+				$(".panelGDSNReason").show();
+				this.onNonGDSNReasonChange();
+
+			} else {
+				$("#lblType").text("GDSN");
+				$(".nonGdsn").hide();
+				$(".panelGDSNReason").hide();
+				$(".panelGDSNReasonJust").hide();
+			}
+
+			this.resetReservationPanel();
+			this.toogleGtinType($("#btnOT"), "btnAW");
+
+		},
+
+		toogleGtinType: function (btn, otherBtnId) {
+
+			if ($(btn).hasClass("btn-default")) {
+				//switch back to default
+				$(btn).removeClass("btn-default");
+				$(btn).addClass("btn-primary");
+				$("#" + otherBtnId).removeClass("btn-primary");
+				$("#" + otherBtnId).addClass("btn-default");
+			}
+
+			//update UoM                
+			if (otherBtnId === "btnAW") {
+				//Active one is Others
+				$("#lblGtinType").text("Others");
+
+				//prepare reserve panel
+				$("#btnKG").hide();
+				$("#btnEA").show();
+				$("#btnEA").addClass("btn-default");
+				$("#btnEA").addClass("btn-primary");
+				$("#btnEA_alt").hide();
+
+			} else {
+				//Active one is Average weight
+				$("#lblGtinType").text("Average weight");
+
+				//prepare reserve panel
+				$("#btnEA").hide();
+				$("#btnKG").show();
+				$("#btnKG").addClass("btn-default");
+				$("#btnKG").addClass("btn-primary");
+				$("#btnEA_alt").show();
+			}
+
+			//force ip toogle to reevaluate it
+			this.toogleIP();
+
+			//always show alt UoM
+			$(".pUoM").show();
+		},
+
+		toogleIP: function () {
+			var includesIP = $($("#chkIp")).is(':checked');
+
+			if (includesIP) {
+				$("#lblGtinType").append(" (IP included)");
+				$("#btnIP_alt").show();
+			} else {
+				$("#lblGtinType").text($("#lblGtinType").text().replaceAll(" (IP included)", ""));
+				$("#btnIP_alt").hide();
+			}
+
+		},
+
+		onNonGDSNReasonChange: function () {
+			var val = $("#selReason").val();
+
+			$("#lblNonGdsnReason").text(val);
+			if (val === "Other") {
+				$(".panelGDSNReasonJust").show();
+				$("#lblNonGdsnReasonJust").show();
+			} else {
+				$(".panelGDSNReasonJust").hide();
+				$("#lblNonGdsnReasonJust").hide();
+			}
+		},
+
+		onNonGDSNReasonJustificationUpdated: function (value) {
+			$("#lblNonGdsnReasonJust").text(value);
+		},
+
+		toGtinInput: function (toInputMode) {
+
+			if (toInputMode) {
+				$("#panelGtinInput").show();
+				$("#panelGtinRes").hide();
+				$("#lblTitleGtin").text("GTINs for creation");
+				$("#lblGtinInput").text("-");
+				$(".gtinInputPanel2").show();
+				this.removeAllGtins();
+
+			} else {
+				//reserve gtin
+				$("#panelGtinInput").hide();
+				$(".gtinInputPanel2").hide();
+				$("#panelGtinRes").show();
+				$("#lblTitleGtin").text("Reserve GTINs for");
+
+				var isGDSNFlow = $("#btnGS1").hasClass("btn-primary");
+				if (isGDSNFlow) {
+					this.reserveGtinsFor = [];
+					this.resetReservationPanel();
+				} else if ($("#lblGtinType").text().indexOf("Others") === 0) {
+					//     //non-gdsn - Others        
+					this.reserveGtinsFor = ["EA"];
+				} else {
+					//non-gdsn - Average weight                     
+					this.reserveGtinsFor = ["KG"];
+				}
+
+				$("#lblGtinInput").text(this.reserveGtinsFor.length === 0 ? "-" : this.reserveGtinsFor[0]);
+				$("#lblGtinInput").show();
+				$(".lblGtin").hide();
+			}
+
+			this.prepareSuppliersForGTINInput(toInputMode);
+		},
+
+		resetReservationPanel: function () {
+			$("#btnEA").removeClass("btn-primary");
+			$("#btnKG").removeClass("btn-primary");
+			$("#btnEA").addClass("btn-default");
+			$("#btnKG").addClass("btn-default");
+
+			$(".pUoM button").each(function () {
+				$(this).removeClass("btn-primary");
+				$(this).addClass("btn-default");
+			});
+
+			$("#btnEA").show();
+			$("#btnKG").show();
+			$(".pUoM").hide();
+		},
+
+		prepareSuppliersForGTINInput: function (toInputMode) {
+			$(".supRow").hide();
+
+			if (toInputMode) {
+				//hide search of supplier, provide one by default, hide remove all button, 
+				$("#panelSupplierSearch").hide();
+				$("#panelSupplierDetails").show();
+				$("#btnRemoveAllSup").hide();
+				$(".supMain").show();
+				$("#btnRemoveMainSup").hide();
+
+			} else {
+				$("#panelSupplierSearch").show();
+				$("#panelSupplierDetails").hide();
+				$("#btnRemoveMainSup").show();
+				$("#btnRemoveAllSup").show();
+			}
+		},
+
+		toogleBaseUoM: function (btn, otherBtnId) {
+			$(".pUoM").show();
+
+			if ($(btn).hasClass("btn-default")) {
+				//switch back to default
+				$(btn).removeClass("btn-default");
+				$(btn).addClass("btn-primary");
+				$("#" + otherBtnId).removeClass("btn-primary");
+				$("#" + otherBtnId).addClass("btn-default");
+			}
+
+			//update UoM                
+			if (otherBtnId === "btnEA") {
+				//Active one is KG
+				$("#btnEA_alt").show();
+				//$("#btnIP_alt").hide();                   
+			} else {
+				$("#btnEA_alt").hide();
+				//$("#btnIP_alt").show();                                       
+			}
+
+			$(".pUoM button").each(function () {
+				$(this).removeClass("btn-primary");
+				$(this).addClass("btn-default");
+			});
+
+			//gtin res label                
+			this.reserveGtinsFor = [$(btn).text()];
+			$("#lblGtinInput").text(this.reserveGtinsFor[0]);
+
+		},
+
+		selectUoM: function (btn) {
+			if ($(btn).hasClass("btn-primary")) {
+				//switch back to default
+				$(btn).removeClass("btn-primary");
+				$(btn).addClass("btn-default");
+
+				//remove from lbl selected    
+				// Fix: array pop doesn't take argument to remove specific item in standard JS, but let's keep logic similar or fix it.
+				// Assuming logical intent is to remove the item.
+				var index = this.reserveGtinsFor.indexOf($(btn).text());
+				if (index > -1) {
+					this.reserveGtinsFor.splice(index, 1);
+				}
+			} else {
+				//switch to selected mode
+				$(btn).removeClass("btn-default");
+				$(btn).addClass("btn-primary");
+
+				//add from lbl selected
+				this.reserveGtinsFor.push($(btn).text());
+			}
+
+			$("#lblGtinInput").text(this.reserveGtinsFor);
+
+		},
+
+		removeAllGtins: function () {
+			$(".gtinRow").hide();
+			$(".lblGtin").hide();
+			$("#lblGtinInput").show();
+			$(".lblGtin2").hide();
+			$("#lblGtinInput2").show();
+			$(".supRegInfoPanel").hide();
+
+			$("#panelMassValidation").hide();
+			$("#gtinInput").focus();
+		},
+
+		pasteSingleGtin: function () {
+			var row = $('.gtinRow:hidden').first();
+			row.show();
+
+			if (row.hasClass("gtinValid")) {
+				if (row.hasClass("gtinCreation")) {
+					//creation
+					$('.lblGtin:hidden').first().show();
+					$("#lblGtinInput").hide();
+				} else {
+					//reactivation
+					$('.lblGtin2:hidden').first().show();
+					$("#lblGtinInput2").hide();
+				}
+			}
+
+			$("#panelMassValidation").show();
+			$(".supRegInfoPanel").show();
+			this.doOnGtinListUpdate();
+
+			$("#gtinInput").val("");
+			$("#gtinInput").focus();
+		},
+
+		pasteGtins: function () {
+			$(".gtinRow").show();
+			$("#panelMassValidation").show();
+			$(".lblGtin").show();
+			$("#lblGtinInput").hide();
+			$("#lblGtinInput2").hide();
+			$(".supRegInfoPanel").show();
+		},
+
+		removeAllSuppliers: function () {
+			$(".supRow").hide();
+			$("#panelSupplierDetails").hide();
+			$("#supInput").focus();
+			$(".supRegInfoPanel").hide();
+			$(".supNoRegInfoPanel").hide();
+		},
+
+		showGtins: function (btn, action) {
+			$(btn).addClass("btn-primary").removeClass("btn-default");
+			$(".btnSwitchGtins:not(#" + $(btn).attr("id") + ")").removeClass("btn-primary").addClass("btn-default");
+
+			if (action === 0) {
+				//ALL
+				$(".gtinValid").show();
+				$(".gtinInvalid").show();
+			} else if (action === 1) {
+				//VALID
+				$(".gtinValid").show();
+				$(".gtinInvalid").hide();
+			} else {
+				//VALID
+				$(".gtinValid").hide();
+				$(".gtinInvalid").show();
+			}
+
+			//$("#datatable-gi").DataTable().draw()
+
+		},
+
+		reuse: function (index) {
+			$(".fake-" + index).show();
+			$(".orig-" + index).hide();
+			$("#lblGtinInput").hide();
+		},
+
+		undoReuse: function (index) {
+			$(".fake-" + index).hide();
+			$(".orig-" + index).show();
+			$("#lblGtinInput2").hide();
+		}
+
+	};
+
+	App.CreationCreateTasks = CreationCreateTasks;
+
+})(jQuery);
