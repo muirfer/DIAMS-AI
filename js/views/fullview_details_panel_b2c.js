9c47e71b9d351a7ace0babbce181cbf78ed4b029
Fernando Muir <muirfer@gmail.com>
2026-07-16 21:07:32 +0200

    Creation - DS- Nutrition values

--- js/views/fullview_details_panel_b2c.js
+++ js/views/fullview_details_panel_b2c.js
@@ -10,3 +10,13 @@ function changeNutriscore(nutriId) {
     var selectedValue = $('input[name="nutri-' + nutriId + '"]:checked').val();
     $('#nutri-score-' + nutriId + ' label:has(input[value="' + selectedValue + '"])').addClass('selected-score');
 }
+
+function showOrHideNutrientInput(elem, nId) {
+    if ($(elem).is(':checked')) {
+        $("." + nId).show()
+        $(".l" + nId).css("opacity", "1")
+    } else {
+        $("." + nId).hide()
+        $(".l" + nId).css("opacity", "0.6")
+    }
+}
